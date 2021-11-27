1187ece18ac1ea39792d6659c2074b41e668064d
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-11-27 10:21:43 -0500

    Update Kewords.hpp

--- Const/Kewords.hpp
+++ Const/Kewords.hpp
@@ -1,36 +1,120 @@
 #pragma once
+#include "../pch.h"
 
-static std::map<std::string, std::vector<char>> keywords {
-	// write to regesters
-	{"ldwa", {'i', 'd'}},
-	{"ldwx", {'i', 'd'}},
+namespace KeywordMap {
+	enum struct keywords {
+		ldwa,
+		ldwx,
 
-	// store regesters in memory
-	{"stwa", {'d'}},
-	{"stwx", {'d'}},
+		stwa,
+		stwx,
 
-	// compare regesters
-	{"cpwa", {'i', 'd'}},
-	{"cpwx", {'i', 'd'}},
+		block,
 
-	// arithmetic on regesters
-	{"adda", {'i', 'd'}},
-	{"addx", {'i', 'd'}},
+		cpwa,
+		cpwx,
 
-	// branching instructions
-	{"br", {1}},
-	{"brgt", {1}},
-	{"brlt", {1}},
-	{"breq", {1}},
-	{"brne", {1}},
-	{"brge", {1}},
-	{"brle", {1}},
+		adda,
+		addx,
+		suba,
+		subx,
 
-	// input
-	{"deci", {'d'}},
-	{"stri", {'d'}},
+		br,
+		brgt,
+		brlt,
+		breq,
+		brne,
+		brge,
+		brle,
 
-	// output
-	{"deco", {1}},
-	{"stro", {1}}
-};
+		deci,
+		stri,
+
+		deco,
+		stro,
+
+		stop,
+		end
+	};
+
+	static std::map<std::string, KeywordMap::keywords> keywordMap {
+		{"ldwa", keywords::ldwa},
+		{"ldwx", keywords::ldwx},
+
+		{"stwa", keywords::stwa},
+		{"stwx", keywords::stwx},
+
+		{".block", keywords::block},
+		
+		{"cpwa", keywords::cpwa},
+		{"cpwx", keywords::cpwx},
+
+		{"adda", keywords::adda},
+		{"addx", keywords::addx},
+		{"suba", keywords::suba},
+		{"subx", keywords::subx},
+
+		{"br", keywords::br},
+		{"brgt", keywords::brgt},
+		{"brlt", keywords::brlt},
+		{"breq", keywords::breq},
+		{"brne", keywords::brne},
+		{"brge", keywords::brge},
+		{"brle", keywords::brle},
+
+		{"deci", keywords::deci},
+		{"stri", keywords::stri},
+
+		{"deco", keywords::deco},
+		{"stro", keywords::stro},
+
+		{"stop", keywords::stop},
+		{".end", keywords::end}
+	};
+}
+
+namespace StrMap {
+	static std::map<std::string, std::vector<char>> keywordMap {
+		// write to regesters
+		{"ldwa", {'i', 'd'}},
+		{"ldwx", {'i', 'd'}},
+
+		// store regesters in memory
+		{"stwa", {'d'}},
+		{"stwx", {'d'}},
+
+		// create blocks of memory
+		{".block", {1}},
+
+		// compare regesters
+		{"cpwa", {'i', 'd'}},
+		{"cpwx", {'i', 'd'}},
+
+		// arithmetic on regesters
+		{"adda", {'i', 'd'}},
+		{"addx", {'i', 'd'}},
+		{"suba", {'i', 'd'}},
+		{"subx", {'i', 'd'}},
+
+		// branching instructions
+		{"br", {1}},
+		{"brgt", {1}},
+		{"brlt", {1}},
+		{"breq", {1}},
+		{"brne", {1}},
+		{"brge", {1}},
+		{"brle", {1}},
+
+		// input
+		{"deci", {'d'}},
+		{"stri", {'d'}},
+
+		// output
+		{"deco", {1}},
+		{"stro", {1}},
+
+		// ending program
+		{"stop", {1}},
+		{".end", {1}}
+	};
+}
