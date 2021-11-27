ad08605262aee677c93d44bc3266cb899ab528ea
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-11-27 14:30:12 -0500

    Update Assemble.cpp

--- Assemble/Assemble.cpp
+++ Assemble/Assemble.cpp
@@ -26,29 +26,79 @@ Assemble::Assemble(std::fstream& file) {
 	file.close();
 }
 
-bool Assemble::isLineLegal(std::string& codeLine) {
-	const size_t size = codeLine.size();
-	size_t rindex = size;
+void Assemble::whitespaceRemover(std::string& str, const size_t& pos=1) const {
+	switch(pos) { // remove trailing and begining whitespace(s)
+		case 3: {
+			auto itr = str.rbegin();
+			size_t rindex = str.size();
 
-	{ // remove trailing whitespace(s)
-		auto itr = codeLine.rbegin();
-		while(*itr == ' ') {
-			rindex--;
-			itr++;
+			while(*itr == ' ') {
+				rindex--;
+				itr++;
+			}
+
+			str = str.substr(0, rindex);
+
+			auto b_itr = str.begin();
+			size_t index = 0;
+
+			while(*b_itr == ' ') {
+				index++;
+				b_itr++;
+			}
+
+			str = str.substr(index);
+			break;
 		}
 
-		codeLine = codeLine.substr(0, rindex);
+		case 2: {// remove trailing whitespaces
+			auto itr = str.rbegin();
+			size_t rindex = str.size();
+
+			while(*itr == ' ') {
+				rindex--;
+				itr++;
+			}
+
+			str = str.substr(0, rindex);
+			break;
+		}
+		default: // remove begining whitespaces
+			auto b_itr = str.begin();
+			size_t index = 0;
+			while(*b_itr == ' ') {
+				index++;
+				b_itr++;
+			}
+
+			str = str.substr(index);
+	}
+}
+
+bool Assemble::isLineLegal(std::string& codeLine) {
+	whitespaceRemover(codeLine, 3);
+	size_t commandIndex = codeLine.find(' ');
+	std::string command = codeLine.substr(0, commandIndex);
+
+	if(StrMap::keywordMap[command].size() == 0) {
+		commandIndex = codeLine.find(' ', command.size()-1);
+		const size_t stop = codeLine.find(' ', commandIndex)-1;
+		command = codeLine.substr(commandIndex, stop);
+		whitespaceRemover(command, 3);
 	}
 
-	const size_t commandIndex = codeLine.find(' ');
-	std::string command = codeLine.substr(0, commandIndex);
 	// largest size of vector: 2
 	const std::vector<char> accepts = StrMap::keywordMap[command];
 
 	if(accepts.size() < 1) { // command not found
 		return 0;
 	}
+	
+	if(accepts[0] == 1) {
+		return 1;
+	}
 
+	const size_t size = codeLine.size();
 	const char back = std::tolower(codeLine.back());
 	for(char ii : accepts) {
 		if(back == ii) { // command found
@@ -59,15 +109,15 @@ bool Assemble::isLineLegal(std::string& codeLine) {
 		}
 	}
 
-	command[0] = std::tolower(command[0]);
-	if((command[0] == 'b') || (command[0] == 's') || (command[0] == 'd')) {
-		code.insert(std::pair<KeywordMap::keywords, std::string>(
-			KeywordMap::keywordMap[command], codeLine.substr(commandIndex+1,
-				size)));
-		return 1;
-	}
-
 	return 0;
+}
+
+void Assemble::assemble(void) {
+	auto itr = code.begin();
+
+	while(itr != code.end()) {
+		
+	}
 }
 
 void Assemble::throwError(const Errors& error, const std::string& codeLine,
