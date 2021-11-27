66f4d16be8c9983c9b619837a86b922a5256163b
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-11-27 10:22:47 -0500

    Update Assemble.hpp

--- Assemble/Assemble.hpp
+++ Assemble/Assemble.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include "..\pch.h"
 #include "..\Const\Errors.hpp"
+#include "..\Const\Kewords.hpp"
+
+using namespace KeywordMap;
 
 class Assemble {
 private:
-	std::map<std::string*, int> code;
+	// key = command, value = instruction
+	std::map<KeywordMap::keywords, std::string> code;
 
 	/*
 	* Determin if the current line of code is legal, or not.
@@ -12,15 +16,17 @@ private:
 	* @param codeLine - The line of code being checked
 	* @returns - true if the line is legal else false
 	*/
-	bool isLineLegal(std::string&) const;
+	bool isLineLegal(std::string&);
 
 	/*
 	* If a line of code is not legal (see isLineLegal function) then throw an
 	error
 	*
 	* @param error - Error used to generate error message
+	* @param codeLine - Line of code that caused the error
+	* @param lineNum - Line number where error occurred
 	*/
-	void throwError(const Errors&) const;
+	void throwError(const Errors&, const std::string&, const int&) const;
 
 public:
 	Assemble(std::fstream&);
