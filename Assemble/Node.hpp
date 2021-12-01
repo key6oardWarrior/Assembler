a90f3c2508364050ea5aa5f088b6739e440753d8
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-11-30 22:59:21 -0500

    Update Node.hpp

--- Assemble/Node.hpp
+++ Assemble/Node.hpp
@@ -8,16 +8,5 @@ struct Node {
 	bool isGo2 = 0; // if Node has a left branch, or is left branch
 
 	Node(void) = default;
-
-	~Node(void) {
-		if(left == NULL) {
-			delete left;
-			left = nullptr;
-		}
-
-		if(right == NULL) {
-			delete right;
-			right = nullptr;
-		}
-	}
+	~Node(void) = default;
 };
