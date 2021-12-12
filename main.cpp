f84cb72d205136e3499d79375641a515e48fe322
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-12-12 13:15:43 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "pch.h"
-#include "Assemble.hpp"
+#include "Assemble\Assemble.hpp"
 
 int main(void) {
 	std::cout << "Enter assembly file: ";
@@ -7,25 +7,37 @@ int main(void) {
 	std::string file;
 	std::getline(std::cin, file);
 
-	std::fstream io = std::fstream(file);
-	Assemble assembled = Assemble(io);
-
 	Assemble* assembled = NULL;
 	char answer;
 	do {
-		std::cout << "\nAssemble (a), Run (r), Exit (e): ";
+		std::cout << "\nAssemble (a), Run (r), Reassemble (ra), \
+Assemble new file (nf), Exit (e): ";
 		std::cin >> answer;
 		answer = std::tolower(answer);
 
 		switch(answer) {
 			case 'a':
-				assembled = new Assemble(io);
-				assembled->assemble();
+				assembled = new Assemble(file);
+				assembled->assembleCode();
 				break;
+
 			case 'r':
-				assembled = new Assemble(io);
-				assembled->assemble();
+				assembled = new Assemble(file);
+				assembled->assembleCode();
 				break;
+
+			case 'ra':
+				delete assembled;
+				assembled = new Assemble(file);
+				assembled->assembleCode();
+				break;
+
+			case 'nf':
+				delete assembled;
+				std::getline(std::cin, file);
+
+				assembled = new Assemble(file);
+				assembled->assembleCode();
 			default:
 				break;
 		}
